e89ae9196ded5b750b3e53d5a8ce3b74d0e02ab2
abdillah-core <abdillah.firdaus01@gmail.com>
2025-05-18 14:35:31 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ if uploaded_file:
     results.append({"ASAL": "Total", "Nominal Pengurangan": formatted_total_all})
 
     # Tampilkan hasil
-    result_df = pd.DataFrame(results).astype(str)
+    result_df = pd.DataFrame(results)
+    result_df["Nominal Pengurangan"] = pd.to_numeric(result_df["Nominal Pengurangan"], errors="coerce")
     st.subheader(f"Rekap Nominal Pengurangan dari Jam 00:00 - 08:00 Tanggal {selected_date.strftime('%d %B %Y')}")
     st.table(result_df)
 
